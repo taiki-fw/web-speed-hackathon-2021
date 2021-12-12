8e63a514b35626fa94d0549d66972ddea81236af
taiki-fw <taiki_52@outlook.jp>
2021-12-12 20:47:54 +0900

    fix:env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,7 @@ const config = {
       path.resolve(SRC_PATH, './index.jsx'),
     ],
   },
-  mode: 'none',
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
@@ -64,7 +64,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: process.env.NODE_ENV,
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
