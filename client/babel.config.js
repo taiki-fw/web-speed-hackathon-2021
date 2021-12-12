7aa6f9dd2514c0e90f8602b0642b35df7af3f87a
taiki-fw <taiki_52@outlook.jp>
2021-12-13 02:32:21 +0900

    fix:react env

--- client/babel.config.js
+++ client/babel.config.js
@@ -7,11 +7,6 @@ module.exports = {
         useBuiltIns: false,
       },
     ],
-    [
-      '@babel/preset-react',
-      {
-        development: true,
-      },
-    ],
+    ['@babel/preset-react'],
   ],
 };
