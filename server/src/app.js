9597027d2bd943227ab403d4018b366280df7470
taiki-fw <taiki_52@outlook.jp>
2021-12-19 02:49:23 +0900

    :bug:gzip

--- server/src/app.js
+++ server/src/app.js
@@ -18,19 +18,12 @@ app.use(
     secret: 'secret',
   }),
 );
+
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
 
-app.use((_req, res, next) => {
-  res.header({
-    'Cache-Control': 'max-age=0, no-transform',
-    Connection: 'close',
-  });
-  return next();
-});
-
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
-app.use(compression());
 
 export { app };
